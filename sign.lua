50b4cfb529ef63f852133c5d38e68854ea05d132
PainXpress <124521660+PainXpress@users.noreply.github.com>
2025-05-01 10:01:42 -0500

    Update sign.lua

--- sign.lua
+++ sign.lua
@@ -1,58 +1,146 @@
+-- GearHallow Casino Flashy Sign
+
 local mon = peripheral.find("monitor")
-assert(mon, "Monitor not found")
-term.redirect(mon)
-
 mon.setTextScale(0.5)
-term.setBackgroundColor(colors.black)
+mon.setBackgroundColor(colors.black)
+local monW, monH = mon.getSize()
 
--- Rainbow colors to cycle through
-local rainbow = {
-  colors.red, colors.orange, colors.yellow, colors.lime,
-  colors.cyan, colors.blue, colors.purple, colors.pink
+-- Basic 5x5 font (use '#' for pixels)
+font = {
+    ["A"] = {
+        " ### ",
+        "#   #",
+        "#####",
+        "#   #",
+        "#   #",
+    },
+    ["C"] = {
+        " ### ",
+        "#    ",
+        "#    ",
+        "#    ",
+        " ### ",
+    },
+    ["E"] = {
+        "#####",
+        "#    ",
+        "#### ",
+        "#    ",
+        "#####",
+    },
+    ["G"] = {
+        " ### ",
+        "#    ",
+        "#  ##",
+        "#   #",
+        " ### ",
+    },
+    ["H"] = {
+        "#   #",
+        "#   #",
+        "#####",
+        "#   #",
+        "#   #",
+    },
+    ["I"] = {
+        " ### ",
+        "  #  ",
+        "  #  ",
+        "  #  ",
+        " ### ",
+    },
+    ["L"] = {
+        "#    ",
+        "#    ",
+        "#    ",
+        "#    ",
+        "#####",
+    },
+    ["N"] = {
+        "#   #",
+        "##  #",
+        "# # #",
+        "#  ##",
+        "#   #",
+    },
+    ["O"] = {
+        " ### ",
+        "#   #",
+        "#   #",
+        "#   #",
+        " ### ",
+    },
+    ["R"] = {
+        "#### ",
+        "#   #",
+        "#### ",
+        "#  # ",
+        "#   #",
+    },
+    ["S"] = {
+        " ####",
+        "#    ",
+        " ### ",
+        "    #",
+        "#### ",
+    },
+    ["W"] = {
+        "#   #",
+        "#   #",
+        "# # #",
+        "## ##",
+        "#   #",
+    },
+    [" "] = {
+        "     ",
+        "     ",
+        "     ",
+        "     ",
+        "     ",
+    }
 }
 
--- 5x5 block font definition
-local font = {
-  ["A"] = {" ### ","#   #","#####","#   #","#   #"},
-  ["C"] = {" ### ","#   #","#    ","#   #"," ### "},
-  ["E"] = {"#####","#    ","#### ","#    ","#####"},
-  ["G"] = {" ### ","#    ","# ## ","#  # "," ### "},
-  ["H"] = {"#   #","#   #","#####","#   #","#   #"},
-  ["I"] = {"#####","  #  ","  #  ","  #  ","#####"},
-  ["L"] = {"#    ","#    ","#    ","#    ","#####"},
-  ["N"] = {"#   #","##  #","# # #","#  ##","#   #"},
-  ["O"] = {" ### ","#   #","#   #","#   #"," ### "},
-  ["R"] = {"#### ","#   #","#### ","#  # ","#   #"},
-  ["S"] = {" ####","#    "," ### ","    #","#### "},
-  ["W"] = {"#   #","#   #","# # #","# # #"," # # "},
-  [" "] = {"     ","     ","     ","     ","     "}
-}
-
--- Draws a string in big letters at (x,y) with a given color
-local function drawBigText(x, y, text, color)
-  text = text:upper()
-  for row = 1, 5 do
-    term.setCursorPos(x, y + row - 1)
-    for i = 1, #text do
-      local char = text:sub(i, i)
-      local glyph = font[char] or font[" "]
-      term.setTextColor(color)
-      term.write(glyph[row] .. " ")
+-- Draw a letter at (x, y) with scale and color
+function drawLetter(char, x, y, scale, color)
+    local lines = font[char] or font[" "]
+    for row = 1, #lines do
+        for col = 1, #lines[row] do
+            local pixel = lines[row]:sub(col, col)
+            if pixel == "#" then
+                for dx = 0, scale - 1 do
+                    for dy = 0, scale - 1 do
+                        mon.setCursorPos(x + (col - 1) * scale + dx, y + (row - 1) * scale + dy)
+                        mon.setTextColor(color)
+                        mon.write("█")
+                    end
+                end
+            end
+        end
     end
-  end
 end
 
--- Get screen size
-local w, h = term.getSize()
+-- Draw scaled-up text centered
+function displayText(text, color)
+    mon.clear()
+    local scale = 2
+    local letterWidth = 5 * scale
+    local spacing = 1 * scale
+    local totalWidth = (#text * letterWidth) + ((#text - 1) * spacing)
+    local startX = math.floor((monW - totalWidth) / 2)
+    local startY = math.floor((monH - (5 * scale)) / 2)
 
--- Main animation loop
+    for i = 1, #text do
+        local char = text:sub(i, i):upper()
+        drawLetter(char, startX + (i - 1) * (letterWidth + spacing), startY, scale, color)
+    end
+end
+
+-- Flashy color loop
+local colorsList = { colors.red, colors.orange, colors.yellow, colors.green, colors.cyan, colors.blue, colors.purple, colors.pink }
+
 while true do
-  for i = 1, #rainbow do
-    term.setBackgroundColor(colors.black)
-    term.clear()
-    -- Center "GearHallow" and "Casino"
-    drawBigText(math.floor((w - (#"GEARHALLOW" * 6 - 1)) / 2), 3, "GearHallow", rainbow[i])
-    drawBigText(math.floor((w - (#"CASINO" * 6 - 1)) / 2), 11, "Casino", rainbow[#rainbow - i + 1])
-    sleep(0.3)
-  end
+    for i = 1, #colorsList do
+        displayText("GEARHALLOW", colorsList[i])
+        sleep(0.4)
+    end
 end
